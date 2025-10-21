cb1de8e8d6635d778285face4cababd202e32a3e
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2025-10-21 16:05:33 -0400

    Add CandidateDatabase SO

---
+++ Assets/Scripts/CandidateSystem/CandidateDatabase.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CandidateDatabase", menuName = "Scriptable Objects/CandidateDatabase")]
+public class CandidateDatabase : ScriptableObject {
+    [Header("All Candidates")]
+    public List<Candidate> allCandidates = new List<Candidate>();
+
+    [Header("Body Languages")]
+    public List<BodyLanguageBehavior> bodyLanguages = new List<BodyLanguageBehavior>();
+
+    public Candidate GetRandomCandidate() {
+        if (allCandidates.Count > 0) {
+            return allCandidates[Random.Range(0, allCandidates.Count)];
+        }
+        Debug.LogError("No candidates in database");
+        return null;
+    }
+
+
+}
