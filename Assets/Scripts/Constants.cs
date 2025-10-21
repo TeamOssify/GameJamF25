160c20155dc5f10a2f38b51c595a013006942278
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-10-20 20:53:00 -0400

    Add exit button and days high score to main menu

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,5 @@
+public static class Constants {
+    public static class PlayerPrefsKeys {
+        public const string HIGH_SCORE = "DaysHighScore";
+    }
+}
--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,7 +1,23 @@
+using System.Globalization;
+
+using TMPro;
+
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour {
+    [SerializeField] private GameObject daysHighScoreObject;
+
+    private void Awake() {
+        var daysHighScore = PlayerPrefs.GetInt(Constants.PlayerPrefsKeys.HIGH_SCORE);
+        var daysHighScoreText = daysHighScoreObject.GetComponent<TextMeshProUGUI>();
+        daysHighScoreText.text = string.Format(CultureInfo.InvariantCulture, daysHighScoreText.text, daysHighScore);
+    }
+
     public void PlayButton_OnClick() {
         Debug.Log("PlayButton_OnClick");
+    }
+
+    public void ExitButton_OnClick() {
+        Application.Quit();
     }
 }
