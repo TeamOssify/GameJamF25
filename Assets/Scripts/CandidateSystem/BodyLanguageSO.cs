eb9135323de148195596fd279da52d823df96b8a
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2025-10-21 13:19:35 -0400

    Add BodyLanguageSO

---
+++ Assets/Scripts/CandidateSystem/BodyLanguageSO.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BodyLanguage", menuName = "Scriptable Objects/BodyLanguage")]
+public class BodyLanguageBehavior : ScriptableObject {
+    public string behaviorName;
+    public CandidateEnums.BodyLanguageType type;
+
+
+    public float frequency; // for passive
+
+    public float duration; //how long the frame lasts
+
+    public Sprite frame;
+}
--- Assets/Scripts/CandidateSystem/CandidateDataStructures.cs
+++ Assets/Scripts/CandidateSystem/CandidateDataStructures.cs
@@ -10,5 +10,15 @@ public class CandidateEnums : MonoBehaviour {
         Passive,
         Responsive
     }
+
+    [System.Serializable]
+    public class QuestionResponse {
+        public Question question;
+
+        [TextArea(3, 10)]
+        public string responseText;
+
+        public BodyLanguageBehavior responsiveBodyLanguage;
+    }
     
 }
