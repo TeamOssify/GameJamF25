adc2e40e683bb2603187d134e148ed313cb9804e
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-10-21 21:41:02 -0400

    Button manager working (real)

--- Assets/Scripts/Buttons/ButtonManager.cs
+++ Assets/Scripts/Buttons/ButtonManager.cs
@@ -1,12 +1,73 @@
-using UnityEngine;
+﻿using System.Collections;
 
-public class ButtonManager : MonoBehaviour
-{
-    void Update() {
-        
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public sealed class ButtonManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler {
+    [Range(0, 0.01f)]
+    [SerializeField]
+    private float clickDownAmount = 0.004f;
+    private Vector3 _homePosition;
+    private Vector3 _clickPosition;
+    private Vector3 _lerpTarget;
+    private bool _isLerping;
+
+    [SerializeField] private UnityEvent onClick;
+
+    private void Awake() {
+        _homePosition = transform.position;
+        _clickPosition = transform.position - new Vector3(0, clickDownAmount, 0);
+
+        var physicsRaycaster = FindFirstObjectByType<PhysicsRaycaster>();
+        if (!physicsRaycaster)
+        {
+            Camera.main!.gameObject.AddComponent<PhysicsRaycaster>();
+        }
     }
 
-    void OnMouseDown() {
-        Debug.Log("Gurt");
+    public void OnPointerClick(PointerEventData eventData) {
+        if (eventData.button != PointerEventData.InputButton.Left) {
+            return;
+        }
+
+        onClick?.Invoke();
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        if (eventData.button != PointerEventData.InputButton.Left) {
+            return;
+        }
+
+        _lerpTarget = _clickPosition;
+        if (!_isLerping) {
+            _isLerping = true;
+            StartCoroutine(LerpPosition());
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        if (eventData.button != PointerEventData.InputButton.Left) {
+            return;
+        }
+
+        _lerpTarget = _homePosition;
+        if (!_isLerping) {
+            _isLerping = true;
+            StartCoroutine(LerpPosition());
+        }
+    }
+
+    private IEnumerator LerpPosition() {
+        while (true)
+        {
+            if (Mathf.Abs((transform.position - _lerpTarget).y) < clickDownAmount / 50) {
+                _isLerping = false;
+                yield break;
+            }
+
+            transform.position = Vector3.Lerp(transform.position, _lerpTarget, 0.2f);
+            yield return null;
+        }
     }
 }
