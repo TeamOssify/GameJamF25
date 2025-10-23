17e23bea72fb2762a2bc8fcc2c0b92855bcf0afc
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2025-10-23 17:06:00 -0400

    Link sleep button

--- Assets/Scripts/ShiftReport/ShiftReportManager.cs
+++ Assets/Scripts/ShiftReport/ShiftReportManager.cs
@@ -36,6 +36,7 @@ public class ShiftReportManager : MonoBehaviour {
         if (moneyManager.GetBalance() < 0) {
             //fail the run
             shiftData.ResetAll();
+            loadEventChannel.RaiseEvent(mainScene, true);
         }
         loadEventChannel.RaiseEvent(mainScene, true);
         shiftData.ResetForNextShift();
