227bd7de2fe1af7efe89b34f3e95376b82492ce4
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2025-10-23 15:01:36 -0400

    Connect some data with ShiftReport UI

--- Assets/Scripts/ShiftReport/ShiftReportUI.cs
+++ Assets/Scripts/ShiftReport/ShiftReportUI.cs
@@ -1,8 +1,31 @@
+using TMPro;
+
 using UnityEngine;
 
-public class ShiftReportUI : MonoBehaviour
-{
+public class ShiftReportUI : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI endMessage;
+    [SerializeField] private TextMeshProUGUI dayText;
+
+    [SerializeField] private TextMeshProUGUI salaryText;
+    [SerializeField] private TextMeshProUGUI salaryValueText;
+    [SerializeField] private TextMeshProUGUI savingsValueText;
+    [SerializeField] private TextMeshProUGUI rentCostText;
+
+    [SerializeField] private TextMeshProUGUI correctProcessedText;
+    [SerializeField] private TextMeshProUGUI incorrectProcessedText;
+    [SerializeField] private TextMeshProUGUI totalProcessedText;
+
+    [SerializeField] private ShiftData shiftData;
+    [SerializeField] private MoneyData moneyData;
+
     void Awake() {
+        correctProcessedText.text = shiftData.candidatesProcessedCorrectly.ToString();
+        incorrectProcessedText.text = shiftData.candidatesProcessedIncorrectly.ToString();
+
+        savingsValueText.text = moneyData.currentBalance.ToString();
+    }
+
+    public void SleepButtonPressed() {
 
     }
 }
