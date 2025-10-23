43f2ec0996b2d9031b1c42c5f4b2bd2d4c9df6f6
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-10-22 21:32:02 -0400

    Working dialog baybee

--- Assets/Scripts/Dialog/DialogManager.cs
+++ Assets/Scripts/Dialog/DialogManager.cs
@@ -1,5 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections;
 
-public class DialogManager : MonoBehaviour {
-    
+using UnityEngine;
+
+public sealed class DialogManager : MonoBehaviour {
+    [SerializeField] private DialogEventChannelSO dialogEventChannel;
+
+    private IEnumerator Start() {
+        yield return new WaitForSeconds(1);
+
+        dialogEventChannel.RaiseOnNewDialogMessage(DialogOwner.Candidate, "Its ya boyyy");
+
+        yield return new WaitForSeconds(1);
+
+        dialogEventChannel.RaiseOnNewDialogMessage(DialogOwner.Candidate, "Tell me, what do ya know about the McRib?");
+
+        yield return new WaitForSeconds(1);
+
+        dialogEventChannel.RaiseOnNewDialogMessage(DialogOwner.Player, "Well...");
+        dialogEventChannel.RaiseOnNewPlayerQuestions(new[] {
+            "I know it's a seasonal menu item everywhere in the world, except for Luxembourg who have it as a permanent menu item.",
+            "I know they taste good.",
+            "Honestly I prefer the Big Mac."
+        });
+    }
 }
--- Assets/Scripts/UI/ConversationWindow.cs
+++ Assets/Scripts/UI/ConversationWindow.cs
@@ -1,18 +1,23 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConversationWindow : MonoBehaviour {
     [SerializeField] private DialogEventChannelSO dialogEventChannel;
     [SerializeField] private DialogMessage candidateMessagePrefab;
     [SerializeField] private DialogMessage playerMessagePrefab;
     [SerializeField] private PlayerQuestion questionPrefab;
-    [SerializeField] private Transform windowContents;
+    [SerializeField] private RectTransform windowContents;
 
+    private readonly Queue<(DialogOwner owner, string message)> _dialogQueue = new();
+    private readonly Queue<string> _questionQueue = new();
     private readonly List<PlayerQuestion> _currentQuestions = new();
 
     private void OnEnable() {
+        StartCoroutine(ListenForDialog());
+
         dialogEventChannel.OnNewDialogMessage += OnNewDialogMessage;
         dialogEventChannel.OnNewPlayerQuestions += OnNewPlayerQuestions;
         dialogEventChannel.OnChosenPlayerQuestion += OnChosenPlayerQuestion;
@@ -25,27 +30,60 @@ public class ConversationWindow : MonoBehaviour {
     }
 
     private void OnNewDialogMessage(DialogOwner owner, string message) {
-        var messagePrefab = owner == DialogOwner.Player
-            ? playerMessagePrefab
-            : candidateMessagePrefab;
-
-        var newMessage = Instantiate(messagePrefab, windowContents);
-        newMessage.SetText(message);
+        _dialogQueue.Enqueue((owner, message));
     }
 
     private void OnNewPlayerQuestions(string[] questions) {
         foreach (var question in questions) {
-            var newQuestion = Instantiate(questionPrefab, windowContents);
-
-            newQuestion.SetText(question);
-            newQuestion.Click += QuestionClicked;
-
-            _currentQuestions.Add(newQuestion);
+            _questionQueue.Enqueue(question);
         }
     }
 
     private void OnChosenPlayerQuestion(string question) {
         OnNewDialogMessage(DialogOwner.Player, question);
+    }
+
+    // Hack to fix weird layout issues
+    private IEnumerator ListenForDialog() {
+        while (enabled) {
+            var created = false;
+
+            if (!created && _dialogQueue.TryDequeue(out var dialog)) {
+                InstantiateMessage(dialog.owner, dialog.message);
+                created = true;
+            }
+
+            if (!created && _questionQueue.TryDequeue(out var question)) {
+                InstantiateQuestion(question);
+                created = true;
+            }
+
+            yield return WaitForSecondsCache.Get(0.05f);
+
+            if (created) {
+                LayoutRebuilder.ForceRebuildLayoutImmediate(windowContents);
+            }
+        }
+    }
+
+    private void InstantiateMessage(DialogOwner owner, string message) {
+        var messagePrefab = owner == DialogOwner.Player
+            ? playerMessagePrefab
+            : candidateMessagePrefab;
+
+        var newMessage = Instantiate(messagePrefab, windowContents);
+
+        newMessage.SetText(message);
+    }
+
+    private void InstantiateQuestion(string question)
+    {
+        var newQuestion = Instantiate(questionPrefab, windowContents);
+
+        newQuestion.SetText(question);
+        newQuestion.Click += QuestionClicked;
+
+        _currentQuestions.Add(newQuestion);
     }
 
     private void QuestionClicked(string question) {
