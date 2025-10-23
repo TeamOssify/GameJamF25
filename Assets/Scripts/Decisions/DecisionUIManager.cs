755c8be9504e6a27922909c884b9b3cfbf1f4051
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2025-10-22 21:06:26 -0400

    Add DecisionUIManager functionality

--- Assets/Scripts/Decisions/DecisionUIManager.cs
+++ Assets/Scripts/Decisions/DecisionUIManager.cs
@@ -2,10 +2,32 @@ using TMPro;
 
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Collections.Generic;
 public class DecisionUIManager : MonoBehaviour {
-    [SerializeField] private RectTransform decisionUI;
+    [SerializeField] private GameObject decisionUI;
     [SerializeField] private GameObject candidateCell;
+    [SerializeField] private GameObject cellContainer;
+
+    private List<CandidateCell> _activeCells = new List<CandidateCell>();
+
+    public void AddCandidate(CandidateInstance candidate) {
+        GameObject cellObj = Instantiate(candidateCell, cellContainer.transform);
+        CandidateCell cell = cellObj.GetComponent<CandidateCell>();
+
+        if (cell != null) {
+            cell.Initialize(candidate);
+            _activeCells.Add(cell);
+        }
+    }
+
+    public void ClearCells() {
+        foreach (CandidateCell cell in _activeCells) {
+            if (cell != null) {
+                Destroy(cell.gameObject);
+            }
+        }
+        _activeCells.Clear();
+    }
 
 
 }
