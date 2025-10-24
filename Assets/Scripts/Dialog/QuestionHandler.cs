9898f6a37d28e2905de35ecb923363dd350c0f3c
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-10-24 08:01:54 -0400

    Questions almost work

---
+++ Assets/Scripts/Dialog/QuestionHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public sealed class QuestionHandler : MonoBehaviour {
+    [SerializeField] private DialogEventChannelSO dialogEventChannel;
+    [SerializeField] private DialogTreeType dialogTreeType;
+
+    public void OnClick() {
+        dialogEventChannel.RaiseAskStandardQuestion(dialogTreeType);
+    }
+}
