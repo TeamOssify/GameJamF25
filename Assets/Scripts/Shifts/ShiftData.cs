dc1523e36a6c974ee34adb288b222d4035c4bd85
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2025-10-24 16:01:59 -0400

    Made shiftnumber public

--- Assets/Scripts/Shifts/ShiftData.cs
+++ Assets/Scripts/Shifts/ShiftData.cs
@@ -2,11 +2,11 @@
 
 [CreateAssetMenu(fileName = "ShiftData", menuName = "Scriptable Objects/ShiftData")]
 public class ShiftData : ScriptableObject {
-    private int _shiftNumber;
+    public int shiftNumber;
     public int ShiftNumber {
-        get { return _shiftNumber; }
+        get { return shiftNumber; }
         set {
-            _shiftNumber = value;
+            shiftNumber = value;
 
             // We haven't completed this shift yet
             var shiftMinusOne = value - 1;
